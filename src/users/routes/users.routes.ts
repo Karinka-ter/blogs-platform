1ecda0160814e1423a62a9b3f69c24879fee362d
Karinka_ter <tigrantervartanyan@gmail.com>
2026-07-29 14:42:55 +0300

    lesson5 get users fix1

--- src/users/routes/users.routes.ts
+++ src/users/routes/users.routes.ts
@@ -1,9 +1,13 @@
 import {Router} from "express";
 import {getUsersHandler} from "./handlers/get-users.handler";
 import {USERS_ROUTES} from "../constans/users.paths";
+import {
+    paginationAndSortingValidation
+} from "../../core/middlewares/validator/query-pagination-sorting.validation.middleware";
+import {UserSortField} from "./input/UsersSortField";
 
 
 export const usersRouter = Router({});
 
 usersRouter
-    .get(USERS_ROUTES.ROOT,getUsersHandler)
+    .get(USERS_ROUTES.ROOT,paginationAndSortingValidation(UserSortField),getUsersHandler)
