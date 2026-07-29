7a5c72cd753a389ea1209454dc9fff41657d7dd8
Karinka_ter <tigrantervartanyan@gmail.com>
2026-07-29 15:36:45 +0300

    lesson5 post user fix1

--- src/users/routes/handlers/create-user.handler.ts
+++ src/users/routes/handlers/create-user.handler.ts
@@ -2,10 +2,11 @@ import {Request, Response} from "express";
 import {usersService} from "../../application/users.service";
 import {HttpStatus} from "../../../core/types/http-statuses";
 import {errorsHandler} from "../../../core/errors/errors.handler";
+import {usersQueryRepository} from "../../repositories/users-query.repository";
 
 export const createUserHandler = async (req:Request,res:Response)=>{
     try{
-        const user = usersService.create(req.body)
+        const user = await usersService.create(req.body)
         return res.status(HttpStatus.Created).send(user)
     } catch (e:unknown){
         errorsHandler(e,res)
