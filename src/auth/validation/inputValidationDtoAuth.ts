ef0753644b7267d1533c41d6a21a6d55cc154d1e
Karinka_ter <tigrantervartanyan@gmail.com>
2026-08-11 13:41:11 +0300

    add token 4

--- src/auth/validation/inputValidationDtoAuth.ts
+++ src/auth/validation/inputValidationDtoAuth.ts
@@ -4,9 +4,14 @@ const loginInEmailValidationMiddleware = body('loginOrEmail')
     .exists()
     .isString().withMessage('loginOrEmail must be a string')
     .trim()
+    .isLength({min: 1})
+    .withMessage('Length min 1')
+
 const passwordValidationMiddleware = body('password')
     .exists()
     .isString().withMessage('password must be a string')
     .trim()
+    .isLength({min: 1})
+    .withMessage('Length min 1')
 
 export const inputValidatorDtoAuthMiddleware = [loginInEmailValidationMiddleware, passwordValidationMiddleware];
