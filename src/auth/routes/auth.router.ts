b6b7a8a7cf32ce59c1c7a224638c63c7fa175d99
Karinka_ter <tigrantervartanyan@gmail.com>
2026-08-11 13:45:19 +0300

    add token 5

--- src/auth/routes/auth.router.ts
+++ src/auth/routes/auth.router.ts
@@ -2,8 +2,9 @@ import {Router} from "express";
 import {authLoginHandler} from "./handlers/auth-login.handler";
 import {AUTH_ROUTES} from "../constants/auth.paths";
 import {inputValidatorDtoAuthMiddleware} from "../validation/inputValidationDtoAuth";
+import {inputValidationResultMiddleware} from "../../core/middlewares/validator/input-validation-result.middleware";
 
 export const authRouter = Router({})
 
-authRouter.post(AUTH_ROUTES.LOGIN, inputValidatorDtoAuthMiddleware, authLoginHandler)
+authRouter.post(AUTH_ROUTES.LOGIN, inputValidatorDtoAuthMiddleware,inputValidationResultMiddleware, authLoginHandler)
 // authRouter.get(AUTH_ROUTES.ME, authMeHandler)
