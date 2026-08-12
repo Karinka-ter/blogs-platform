0db4ebec3fc777ee871aba445d544df11f304672
Karinka_ter <tigrantervartanyan@gmail.com>
2026-08-12 16:45:16 +0300

    add getcomment 3

--- src/comments/routes/handlers/get-comment-by-id.handler.ts
+++ src/comments/routes/handlers/get-comment-by-id.handler.ts
@@ -1,8 +1,14 @@
 import {Request, Response} from "express";
 import {commentsQueryRepository} from "../../repositories/comments-query.repository";
 import {HttpStatus} from "../../../core/types/http-statuses";
+import {errorsHandler} from "../../../core/errors/errors.handler";
 
 export const getCommentByIdHandler=async (req:Request<{commentId:string},{},{}>,res:Response)=>{
-    const comment = await commentsQueryRepository.getCommentById(req.params.commentId)
-    res.status(HttpStatus.Ok).send(comment)
+    try{
+        const comment = await commentsQueryRepository.getCommentById(req.params.commentId)
+        res.status(HttpStatus.Ok).send(comment)
+    }catch(err:unknown){
+        errorsHandler(err,res)
+    }
+
 }
